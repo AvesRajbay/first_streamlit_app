52757b80d7837627dbcd79001703ea93331accbe
Aves Rajbay <58557239+AvesRajbay@users.noreply.github.com>
2022-11-03 16:34:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menue')
-streamlit.Text ('Omega 3 & Blueberry Oatmeal')
-streamlit.Text ('Kale,Spinach & Rocket Smoothie')
-streamlit.Text ('Hard-Boiled Free Range Egg')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale,Spinach & Rocket Smoothie')
+streamlit.text ('Hard-Boiled Free Range Egg')
