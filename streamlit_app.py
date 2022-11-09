717594fc32310e1e79bbe2beda0a6c63df345502
Aves Rajbay <58557239+AvesRajbay@users.noreply.github.com>
2022-11-09 12:14:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas 
 import snowflake.connector
 import requests
-fom urlib.errors import URLError
+fom urllib.errors import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menue')
