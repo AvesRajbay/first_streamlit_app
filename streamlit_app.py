72ff75dc88f3b1e561b1b9f946c4e6dac7e85748
Aves Rajbay <58557239+AvesRajbay@users.noreply.github.com>
2022-11-09 12:39:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list)
 
-
-  def get_fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")    
        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        streamlit.dataframe(fruityvice_normalized)
